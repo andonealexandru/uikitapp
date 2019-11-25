8c306cae4b951dfeb1a0fa7529f99986a4db223d
Andone Alexandru <andonealexandru19@yahoo.com>
2019-11-25 21:19:52 +0200

    server setup 4

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ const express = require('express');
 const path = require('path');
 const serveStatic = require('serve-static');
 
-let app = express();
-app.use(serveStatic(__dirname + "/dist"));
+const app = express();
 
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
-  console.log('Listening on port ' + port)
-});
+app.use('/', serveStatic(path.join(__dirname, '/dist')));
+
+const port = process.env.PORT || 8080;
+app.listen(port);
+
+console.log('Listening on port: ' + port);
