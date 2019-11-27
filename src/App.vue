bfd59f46b67ab5a9b84116f25565616991b08a72
Andone Alexandru <andonealexandru19@yahoo.com>
2019-11-27 22:52:15 +0200

    Better sign in and more features

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,15 @@
     <vk-navbar class="nav">
       <vk-navbar-nav>
         <vk-navbar-logo><img src="./assets/logo.png" class="logo"></vk-navbar-logo>
-        <vk-navbar-nav-item title="Home" :active="activeHome" @click="activateHome"></vk-navbar-nav-item>
+        <vk-navbar-nav-item title="Home" :active="$router.currentRoute.name === 'home'" @click="$router.push({ name: 'home' })" />
       </vk-navbar-nav>
 
       <vk-navbar-nav slot="right">
-        <vk-navbar-nav-item title="Sign In" :active="activeSignIn"  @click="activateSignIn"></vk-navbar-nav-item>
-        <vk-navbar-nav-item title="Log In" :active="activeLogIn"  @click="activateLogIn"></vk-navbar-nav-item>
+        <vk-navbar-nav-item title="Register" :active="$router.currentRoute.name === 'signin'"  @click="$router.push({ name: 'signin' })" />
+        <vk-navbar-nav-item title="Log In" :active="$router.currentRoute.name === 'login'"  @click="$router.push({ name: 'login' })" />
       </vk-navbar-nav>
     </vk-navbar>
-    <router-view></router-view>
+    <router-view />
   </div>
 </template>
 
@@ -23,42 +23,9 @@
   padding-right: 15px;
   padding-left: 15px;
 }
-</style>>
 </style>
 
 <script>
 export default {
-  data() {
-    return {
-      activeHome: true,
-      activeSignIn: true,
-      activeLogIn: true
-    }
-  },
-  created: function () {
-    this.activeHome = true
-    this.activeLogIn = false
-    this.activeSignIn = false
-  },
-  methods: {
-    activateHome () {
-      this.activeHome = true
-      this.activeSignIn = false
-      this.activeLogIn = false
-      this.$router.push({ name: 'home' })
-    },
-    activateSignIn () {
-      this.activeHome = false
-      this.activeSignIn = true
-      this.activeLogIn = false
-      this.$router.push({ name: 'signin' })
-    },
-    activateLogIn () {
-      this.activeHome = false
-      this.activeSignIn = false
-      this.activeLogIn = true
-      this.$router.push({ name: 'login' })
-    }
-  }
 }
 </script>
--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -1,47 +1,67 @@
-<template>
-  <div class="uk-padding uk-flex-middle">
+<template xmlns="http://www.w3.org/1999/html">
+  <div class="uk-padding uk-flex-middle uk-dark">
     <div class="uk-width-2-5@m uk-align-center">
-      <vk-card hover>
-        <vk-card-title>Create account</vk-card-title>
-        <form class="uk-card-body">
-          <div class="uk-margin">
-            <div class="uk-inline uk-width-1-1">
-              <span class="uk-form-icon"><vk-icon icon="user"></vk-icon></span>
-              <input class="uk-input" type="text" placeholder="First name">
+      <vk-card hover v-if="showForm">
+        <form @submit.prevent="handleSubmit">
+          <fieldset class="uk-fieldset">
+            <legend class="uk-legend">Create your account</legend>
+            <div class="uk-margin">
+              <div class="uk-inline uk-width-1-1">
+                <span class="uk-form-icon"><vk-icon icon="user" /></span>
+                <input v-model="formData.firstName" class="uk-input" type="text" placeholder="First name" required="required">
+              </div>
             </div>
-          </div>
-          <div class="uk-margin">
-            <div class="uk-inline uk-width-1-1">
-              <span class="uk-form-icon"><vk-icon icon="user"></vk-icon></span>
-              <input class="uk-input" type="text" placeholder="Last name">
+            <div class="uk-margin">
+              <div class="uk-inline uk-width-1-1">
+                <span class="uk-form-icon"><vk-icon icon="user" /></span>
+                <input v-model="formData.lastName" class="uk-input" type="text" placeholder="Last name" required="required">
+              </div>
             </div>
-          </div>
-          <div class="uk-margin">
-            <div class="uk-inline uk-width-1-1">
-              <span class="uk-form-icon"><vk-icon icon="mail"></vk-icon></span>
-              <input class="uk-input" type="text" placeholder="Email">
+            <div class="uk-margin">
+              <div class="uk-inline uk-width-1-1">
+                <span class="uk-form-icon"><vk-icon icon="mail" /></span>
+                <input v-model="formData.email" class="uk-input" type="email" placeholder="Email" required="required">
+              </div>
             </div>
-          </div>
-          <div class="uk-margin">
-            <div class="uk-inline uk-width-1-1">
-              <span class="uk-form-icon"><vk-icon icon="location"></vk-icon></span>
-              <input class="uk-input" type="text" placeholder="Address">
+            <div class="uk-margin">
+              <div class="uk-inline uk-width-1-1">
+                <span class="uk-form-icon"><vk-icon icon="location" /></span>
+                <input v-model="formData.city" class="uk-input" type="text" placeholder="City" required="required">
+              </div>
             </div>
-          </div>
-          <div class="uk-margin">
-            <div class="uk-inline uk-width-1-1">
-              <span class="uk-form-icon"><vk-icon icon="receiver"></vk-icon></span>
-              <input class="uk-input" type="text" placeholder="Phone number">
+            <div class="uk-margin">
+              <div class="uk-inline uk-width-1-1">
+                <span class="uk-form-icon"><vk-icon icon="home" /></span>
+                <input v-model="formData.address" class="uk-input" type="text" placeholder="Address" required="required">
+              </div>
             </div>
-          </div>
-          <div class="uk-margin">
-            <div class="uk-inline uk-width-1-1">
-              <span class="uk-form-icon"><vk-icon icon="user"></vk-icon></span>
-              <input class="uk-input" type="text" placeholder="Lock">
+            <div class="uk-margin">
+              <div class="uk-inline uk-width-1-1">
+                <span class="uk-form-icon"><vk-icon icon="receiver" /></span>
+                <input v-model="formData.phone" class="uk-input" type="tel" placeholder="Phone number" required="required">
+              </div>
             </div>
-          </div>
-          <vk-button class="uk-width-1-1">Submit</vk-button>
+            <div class="uk-margin">
+              <div class="uk-inline uk-width-1-1">
+                <span class="uk-form-icon"><vk-icon icon="lock" /></span>
+                <input v-model="formData.password" class="uk-input" type="password" placeholder="Password" required="required">
+              </div>
+            </div>
+            <div class="uk-margin" v-if="!showSpinner">
+              <div class="uk-inline uk-width-1-1">
+                <input type="submit" class="uk-button uk-button-default uk-width-1-1" value="Register now!">
+              </div>
+            </div>
+            <div class="uk-inline uk-width-1-1" style="height: 45px"  v-if="showSpinner">
+              <vk-spinner class="uk-position-center uk-container" ratio="1.5" />
+            </div>
+          </fieldset>
         </form>
+      </vk-card>
+
+      <vk-card class="uk-text-center" type="blank" v-if="!showForm">
+        <vk-card-title>One more step!</vk-card-title>
+        <p>We need to make sure you used your real email.<br> Check your inbox for confirmation email.</p>
       </vk-card>
     </div>
   </div>
@@ -49,6 +69,30 @@
 
 <script>
 export default {
-  name: 'signin'
+  name: 'signin',
+  data () {
+    return {
+      showSpinner: false,
+      showForm: true,
+      formData: {
+        firstName: '',
+        lastName: '',
+        email: '',
+        city: '',
+        address: '',
+        phone: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    handleSubmit () {
+      this.showSpinner = true;
+      setTimeout(this.afterResponse, 3000);
+    },
+    afterResponse() {
+      this.showForm = false;
+    }
+  }
 }
 </script>
